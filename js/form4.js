562c3c5210002e83a9b3367ee1b509987b42ae86
Eli Angel <elijllo10@hotmail.com>
2021-10-03 00:38:21 -0500

    se cambian validaciones del formulario

--- js/form4.js
+++ js/form4.js
@@ -2,42 +2,42 @@
 function validar_nombre(string) {
     
     var nombre = string;
-    var long_nombre = nombre.trim().length;
     /*var expresion = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$/;*/
     var expresion_especiales = /[@!_/\-]+/;
     var expresion_numeros = /[0-9]+/;
 
-    if (long_nombre == 0 ) {
-        document.getElementById("errorNombre").innerText = '*El nombre es obligatorio';
-        return false;
+    if (nombre == "" || nombre.length == 0) {
+      alert("Nombre obligatorio");
+      console.log ("false")
+      return false;
       }
 
-    if (long_nombre < 4 ) {
-        document.getElementById("errorNombre").innerText = '*El nombre debe tener una longitud mayor a 4';
-        return false;
+    if (nombre.length < 4 ) {
+      alert("El nombre debe tener una logítud mínima de 4 caracteres");
+      console.log ("false")
+      return false;
       }
 
-    if (long_nombre >= 30 ) {
-        document.getElementById("errorNombre").innerText = '*El nombre debe tener una longitud maxima de 30 caracteres';
-        return false;
+    if (nombre.length >= 30 ) {
+      alert("El nombre debe tener una longitud maxima de 30 caracteres");
+      console.log ("false")
+      return false;
       }
 
-    /*if (!expresion.test(nombre)){
-        document.getElementById("errorNombre").innerText = '*No se permiten caracteres especiales ni números';
-        return false;
-      }*/
     if (expresion_especiales.test(nombre)){
-        document.getElementById("errorNombre").innerText = '*No se permiten caracteres especiales';
-        return false;
+      alert("El nombre no debe tener caracteres especiales");
+      console.log ("false")
+      return false;
       }
 
     if (expresion_numeros.test(nombre)){
-        document.getElementById("errorNombre").innerText = '*No se permiten números';
-        return false;
+      alert("El nombre no debe tener números");
+      console.log ("false")
+      return false;
       }
  
-   
-    return true;
+   return true
+    
 }
 
 function validar_contrasenas(string1,string2) {
@@ -45,34 +45,36 @@ function validar_contrasenas(string1,string2) {
     
     var password = string1;
     var confirma_password = string2;
-    var long_pass = password.trim().length;
-    var long_confirma_pass = confirma_password.trim().length;
-   
+     
 
-    if (long_pass == 0 ) {
-        document.getElementById("errorPassword").innerText = '*La contraseña es obligatoria';
-        return false;
+    if (password == "" || password.length == 0  ) {
+      alert("La contraseña es obligatoria");
+      console.log ("false")
+      return false;
       }
 
-    if (long_confirma_pass == 0 ) {
-        document.getElementById("errorConfirmaPassword").innerText = '*Debes confirmar tu contraseña';
-        return false;
+    if (confirma_password == "" || confirma_password.length == 0 ) {
+      console.log ("false")
+      alert("Debes confirmar tu contraseña");
+      return false;
       }
     
-    if (long_pass != long_confirma_pass ) {
-        document.getElementById("errorConfirmaPassword").innerText = '*Las contraseñas no son iguales';
-        return false;
+    if (password.length  != confirma_password.length ) {
+      alert("Las contraseñas no son iguales");
+      console.log ("false")
+      return false;
       }
 
-    if (password.trim() != confirma_password.trim() ) {
-        document.getElementById("errorConfirmaPassword").innerText = '*Las contraseñas no son iguales';
-        return false;
+    if (password != confirma_password ) {
+      alert("Las contraseñas no son iguales");
+      console.log ("false")
+      return false;
       }
 
-    
-   return true;
+  console.log("true")
+  return true;
 
 }
 
-/*module.exports.validar_nombre = validar_nombre;
-module.exports.validar_contrasenas = validar_contrasenas;*/
+module.exports.validar_nombre = validar_nombre;
+module.exports.validar_contrasenas = validar_contrasenas;
