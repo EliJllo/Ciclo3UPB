0244a77913bd153b40152b4ce3fc313333c3383b
Eli Angel <elijllo10@hotmail.com>
2021-10-05 00:33:43 -0500

    ajuste en validación de formulario al 75% de cumplimiento

--- js/form4.js
+++ js/form4.js
@@ -1,34 +1,35 @@
 
 function validar_nombre(string) {
     
-    var nombre = string;
+    var campoNombre = document.getElementById("campoNombre").value;
+    console.log(campoNombre);
     //var expresion = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$/;
     var expresion_especiales = /[@!_/\-]+/;
     var expresion_numeros = /[0-9]+/;
 
     //Debe tener una longitud mayor a 4
-   if (nombre.length < 4 ) {
+   if (campoNombre.length < 4 ) {
       alert("El nombre debe tener una logítud mínima de 4 caracteres");
       console.log ("false")
       return false;
       }
 
     //Debe tener una longitud máxima de 30 caracteres
-    else if (nombre.length >= 30 ) {
+    else if (campoNombre.length >= 30 ) {
       alert("El nombre debe tener una longitud maxima de 30 caracteres");
       console.log ("false")
       return false;
       }
 
     //No puede tener ningún caracter especial como '@','!','-','/'
-    else if (expresion_especiales.test(nombre)){
+    else if (expresion_especiales.test(campoNombre)){
       alert("El nombre no debe tener caracteres especiales");
       console.log ("false")
       return false;
       }
 
     //No puede tener ningún número
-    else if (expresion_numeros.test(nombre)){
+    else if (expresion_numeros.test(campoNombre)){
       alert("El nombre no debe tener números");
       console.log ("false")
       return false;
@@ -46,39 +47,50 @@ function validar_nombre(string) {
 function validar_contrasenas(string1,string2) {
   
     
-    var password = string1;
-    var confirma_password = string2;
+    var campoContrasena = string1; //document.getElementById("campoContrasena").value;
+    console.log(campoContrasena);
+    var campoConfirmarContrasena = string2; //document.getElementById("campoConfirmarContrasena").value;
+    console.log(campoConfirmarContrasena);
      
+    if(campoContrasena === null || campoConfirmarContrasena === null ){
 
-    if (password == "" || password.length == 0  ) {
-      alert("La contraseña es obligatoria");
-      console.log ("false")
-      return false;
-      }
-    
-      else if (confirma_password == "" || confirma_password.length == 0 ) {
-      console.log ("false")
-      alert("Debes confirmar tu contraseña");
-      return false;
-      }
-    
-    else if (password.length  != confirma_password.length ) {
-      alert("Las contraseñas no son iguales");
-      console.log ("false")
-      return false;
-      }
+        alert("La contraseña es obligatoria");
+        console.log ("false")
+        return false;
 
-    else if (password != confirma_password ) {
-      alert("Las contraseñas no son iguales");
-      console.log ("false")
-      return false;
-      }
-    
-      else{ console.log("true")
-      return true;}
+       }else{
+      
+        if (campoContrasena == "" || campoContrasena.length == 0  ) {
+          alert("La contraseña es obligatoria");
+          console.log ("false")
+          return false;
+          }
+        
+          else if (campoConfirmarContrasena == "" || campoConfirmarContrasena.length == 0 ) {
+          console.log ("false")
+          alert("Debes confirmar tu contraseña");
+          return false;
+          }
+        
+        else if (campoContrasena.length  != campoConfirmarContrasena.length ) {
+          alert("Las contraseñas no son iguales");
+          console.log ("false")
+          return false;
+          }
 
+        else if (campoContrasena != campoConfirmarContrasena ) {
+          alert("Las contraseñas no son iguales");
+          console.log ("false")
+          return false;
+          }
+        
+          else{ console.log("true")
+          return true;}
+
+     
+      }
  
 }
 
-//module.exports.validar_nombre = validar_nombre;
-//module.exports.validar_contrasenas = validar_contrasenas;
+module.exports.validar_nombre = validar_nombre;
+module.exports.validar_contrasenas = validar_contrasenas;
